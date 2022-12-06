89bb6ae61475ab22f70d00b1dc8043e15f97100c
RichardOkongo <rirachard19@gmail.com>
2022-12-06 09:35:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,39 +1,23 @@
-// Code your solution in this file!
-// Should return  number of blocks of a given value
-  function distanceFromHqInBlocks (someValue) {
-    if (someValue<42) {
-        return (42-someValue);
-    } else {
-        return (someValue-42);
-    }
+function distanceFromHqInBlocks(location){
+  return Math.abs(location - headQuaters)
+}
+// distanceFromHqInBlocks()
+function distanceFromHqInFeet(distance){
+  return distanceFromHqInBlocks(distance) * 264
+}
+function distanceTravelledInFeet(start,destination){
+  return Math.abs(start - destination) * 264
+}
+function calculatesFarePrice(start, destination){
+  let feet = distanceTravelledInFeet(start,destination)
+  if (feet < 400){
+      return 0;
+  }else if (feet > 400 && feet < 2000){
+      return (feet - 400) * 0.02
+  }else if (feet > 2000 && feet < 2500){
+      return 25
+  }else if(feet > 2500){
+      return "cannot travel that far"
   }
-  function distanceFromHqInFeet(someValue) {
-    return distanceFromHqInBlocks (someValue) *264
-  }
-  function distanceTravelledInFeet(start, destination) {
-    if (start>destination){
-        return (start-destination) *264;
-    } else if (destination>start) {
-        return (destination-start) *264;
-    }
+}
 
-  }
-function calculatesFarePrice(start, destination) {
-    let distance = distanceTravelledInFeet(start, destination)
-    if (distance <400) {
-        return ("free")
-    } if (distance >400 && distance<= 2000) {
-        return (distance-400)* 0.02
-        } 
-     if (distance > 2000 && distance <2500) {
-        return("flat fare")
-
-     }  
-     if (distance >2500) {
-        return ("cannot travel that far")
-     } 
-
-    }
-// calculates the distance from Hq block in feet
-// calculates the distance travelled in feet by* by 264
-//calculates  the fare price
